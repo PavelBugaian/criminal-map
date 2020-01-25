1387d8f095030fc1a209e273be3c91cca61be4f2
Paul Bugaian <shebster603@gmail.com>
2020-01-25 19:20:25 +0200

    Implemented getAddress method

--- app/src/main/java/com/example/criminal_map/MainActivity.kt
+++ app/src/main/java/com/example/criminal_map/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.criminal_map
 
+import android.content.res.Resources
 import android.graphics.Canvas
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.drawToBitmap
 import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
+import org.json.JSONObject
 import org.osmdroid.config.Configuration
 import org.osmdroid.gpkg.overlay.features.MarkerOptions
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,17 +30,13 @@ class MainActivity : AppCompatActivity() {
 
         map.setMultiTouchControls(true)
 
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
         val mapController = map.controller
-        mapController.setZoom(14.0)
+        mapController.setZoom(13.5)
         val startPoint = GeoPoint(47.003670, 28.907089)
         mapController.setCenter(startPoint)
 
-        map.minZoomLevel = 14.0
-
-        val marker = Marker(map)
-        marker.position = GeoPoint(47.003670, 28.907089)
-        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        map.overlays.add(marker)
+        map.minZoomLevel = 13.5
     }
 
     override fun onResume() {
@@ -45,5 +47,23 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         map.onPause()
+    }
+
+    fun addMarker(gp : GeoPoint) {
+        val marker = Marker(map)
+        marker.position = gp
+        marker.icon = this@MainActivity.resources.getDrawable(R.drawable.circle, null)
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map.overlays.add(marker)
+    }
+
+    fun getDistrict(address: String): String {
+        return JSONArray(URL("https://nominatim.openstreetmap.org/search/seatch" +
+                "?city=Chisinau&format=json&street=$address").readText())
+                .getJSONObject(1)
+                .getJSONObject("display_name")
+                .toString()
+                .substringAfter("Sectorul ")
+                .substringBefore(",")
     }
 }
